d19d9c1f56f9198d49017d6ceccce3eee36f2f9c
Baoguoq1ang <18701094030@163.com>
2022-08-18 15:22:38 +0800

    update alias

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,21 @@ import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 import Unocss from 'unocss/vite'
-
+// import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [
     vue(),
     Components({
       resolvers: [AntDesignVueResolver()],
     }),
     Unocss({ /* options */ }),
-  ]
+  ],
+  resolve: {
+    // alias: {
+    //   '@': path.resolve(__dirname, 'src')
+    // },
+    // extensions: ['.vue', '.json', '.ts']
+  }
 })
