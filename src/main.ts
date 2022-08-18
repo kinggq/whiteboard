4cfbdb738e6f2b66be6ab2b46971d649d9826799
Baoguoq1ang <18701094030@163.com>
2022-08-18 15:24:36 +0800

    feat: add router & pinia

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import router from './router'
 import './styles/index.css'
 import 'uno.css'
 import './style.css'
 import App from './App.vue'
 
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.mount('#app')
