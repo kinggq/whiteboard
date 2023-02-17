f92982f0216f70b9ecb67c9189360d5b839e5502
Baoguoq1ang <18701094030@163.com>
2023-02-17 17:16:18 +0800

    feat: 修改登录校验规则

--- src/utils/form/rule.ts
+++ src/utils/form/rule.ts
@@ -7,10 +7,10 @@ function createRequireFormRule(message = '不能为空') {
 export const formRule = {
     username: [
         createRequireFormRule('请输入用户名'),
-        { pattern: REGEXP_USER_NAME, message: '用户名格式错误', trigger: 'input' }
+        { /* pattern: REGEXP_USER_NAME, */ message: '用户名格式错误', trigger: 'input' }
     ],
     password: [
         createRequireFormRule('请输入密码'),
-        { pattern: PEGEXP_USER_NAME, message: '密码为6-18位数字/字符/符号的组合', trigger: 'input' }
+        { /* pattern: PEGEXP_USER_NAME, */ message: '密码为6-18位数字/字符/符号的组合', trigger: 'input' }
     ]
 }
