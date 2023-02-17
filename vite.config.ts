62abe3377b37daa5426b4d16e4645f965d881018
Baoguoq1ang <18701094030@163.com>
2023-02-17 17:02:41 +0800

    refactor: 重构unocss配置文件

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 import Unocss from 'unocss/vite'
-import { presetUno, presetAttributify, presetIcons } from 'unocss'
+
 // import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,24 +19,6 @@ export default defineConfig({
     Components({
       resolvers: [NaiveUiResolver()],
     }),
-    Unocss({
-      presets: [
-        presetUno(),
-        presetAttributify(),
-        presetIcons()
-      ],
-      theme: {
-        brand: {
-          
-        },
-        colors: {
-            primary: 'var(--primary-color)'
-        }
-      },
-      shortcuts: {
-        'flex-center': 'flex justify-center items-center',
-        'flex-between': 'flex justify-between items-center'
-      }
-    }),
+    Unocss(),
   ]
 })
