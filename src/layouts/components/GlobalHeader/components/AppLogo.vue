dee1b5503ba35e4b6b7bd008b95e6203d7a6fb1e
Baoguoq1ang <18701094030@163.com>
2023-02-21 14:00:24 +0800

    feat: logo

--- src/layouts/components/GlobalHeader/components/AppLogo.vue
+++ src/layouts/components/GlobalHeader/components/AppLogo.vue
@@ -1,3 +1,8 @@
 <template>
-    <div>logo</div>
+    <router-link to="/" flex-center>
+        <icon-custom-logo text-40px></icon-custom-logo>
+        <h2 pl-10px text-primary>
+            <i>whiteboard</i>
+        </h2>
+    </router-link>
 </template>
