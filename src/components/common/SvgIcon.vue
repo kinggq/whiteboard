2a636a5023d94880e28152a6013c1b7334759021
Baoguoq1ang <18701094030@163.com>
2023-02-21 14:00:59 +0800

    feat: svg utils

---
+++ src/components/common/SvgIcon.vue
@@ -0,0 +1,24 @@
+<template>
+    <svg aria-hidden="true" width="1em" height="1em">
+        <use :xlink:href="symbolId" :fill="color" />
+    </svg>
+</template>
+<script lang="ts" setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+    icon: {
+        type: String,
+        required: true,
+    },
+    prefix: {
+        type: String,
+        default: 'icon-custom',
+    },
+    color: {
+        type: String,
+        default: 'currentColor',
+    },
+})
+const symbolId = computed(() => `#${props.prefix}-${props.icon}`)
+</script>
--- src/utils/common/icon.ts
+++ src/utils/common/icon.ts
@@ -1,7 +1,12 @@
 import { Icon } from '@iconify/vue'
 import { NIcon } from 'naive-ui'
 import { h } from 'vue'
+import SvgIcon from '@/components/common/SvgIcon.vue'
 
 export function renderIcon(icon: string, props = { size: 12}) {
     return () => h(NIcon, props, { default: () => h(Icon, { icon })})
+}
+
+export function renderCustomIcon(icon: string, props = { size: 12 }) {
+    return () => h(NIcon, props, { default: () => h(SvgIcon, { icon }) })
 }
