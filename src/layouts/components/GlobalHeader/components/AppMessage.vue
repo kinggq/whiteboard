73a210a113b0b22ccf1e42e0f4f2fd3c8687bc7f
Baoguoq1ang <18701094030@163.com>
2023-02-21 18:01:03 +0800

    feat: 优化message组件

--- src/layouts/components/GlobalHeader/components/AppMessage.vue
+++ src/layouts/components/GlobalHeader/components/AppMessage.vue
@@ -1,5 +1,31 @@
 <template>
-    <hover-container w-40px content="系统消息">
-        <icon-clarity:bell-line text-20px />
-    </hover-container>
+    <n-popover trigger="click" :show-arrow="false">
+        <template #trigger>
+            <hover-container w-40px relative content="系统消息">
+                <icon-clarity:bell-line text-20px />
+                <n-badge :value="10" :max="9" absolute top--2px right-0px></n-badge>
+            </hover-container>
+        </template>
+        <n-card :bordered="false" title="通知" size="small" header-style="padding: 0;" footer-style="padding: 0;">
+            <template #header-extra>
+                <div>
+                    <n-button text type="primary">
+                        <icon-material-symbols:settings-outline />
+                        通知设置
+                    </n-button>
+                </div>
+            </template>
+            <div w-300px p-20px>
+                暂无消息
+            </div>
+            <template #footer>
+                <div text-center>
+                    <n-button quaternary type="primary">查看全部通知</n-button>
+                </div>
+            </template>
+        </n-card>
+    </n-popover>
 </template>
+<script setup lang="ts">
+
+</script>
