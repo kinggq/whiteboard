5a963882b74d9754473d277fb513144cdb296a55
Baoguoq1ang <18701094030@163.com>
2023-02-23 15:51:14 +0800

    feat: unocss

--- uno.config.ts
+++ uno.config.ts
@@ -38,6 +38,7 @@ export default defineConfig({
       shortcuts: {
         'flex-center': 'flex justify-center items-center',
         'flex-between': 'flex justify-between items-center',
+        'flex-col-center': 'flex-center flex-col',
         'absolute-lb': 'absolute left-0 bottom-0',
       }
 })
