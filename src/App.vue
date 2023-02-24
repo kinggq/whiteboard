db3c7532e60df9bad8da9e30f1d3ec578e36eadf
Baoguoq1ang <18701094030@163.com>
2023-02-24 10:04:44 +0800

    fix: 删除无用文件

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
 import { darkTheme } from 'naive-ui'
 import { themeStore } from './store'
 import { themeJson } from '@/settings'
-import Preference from '@/layouts/preference/preference.vue'
 
 const store = themeStore()
 
@@ -10,10 +9,8 @@ const store = themeStore()
 
 <template>
   <n-config-provider h-full :theme-overrides="themeJson.naiveThemeOverrides" :theme="store.isDark ? darkTheme : undefined">
-    <!-- <n-global-style/> -->
     <naive-provider>
         <router-view></router-view>
-        <Preference v-model:show="store.showPreference" />
     </naive-provider>
   </n-config-provider>
 </template>
