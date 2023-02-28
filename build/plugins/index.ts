7617b7a981afb15397eeefe3f396c35e40d1c217
Baoguoq1ang <18701094030@163.com>
2023-02-28 14:50:46 +0800

    feat: mock配置

--- build/plugins/index.ts
+++ build/plugins/index.ts
@@ -1,9 +1,10 @@
 import unplugin from './unplugin'
 import vue from '@vitejs/plugin-vue'
 import Unocss from 'unocss/vite'
+import mock from './mock'
 
 export function setupVitePlugins() {
-    const plugins = [ vue(), ...unplugin(), Unocss()]
+    const plugins = [ vue(), mock, ...unplugin(), Unocss()]
 
     return plugins
 }
---
+++ build/plugins/mock.ts
@@ -0,0 +1,6 @@
+import { viteMockServe } from 'vite-plugin-mock'
+
+export default viteMockServe({
+    mockPath: 'mock',
+    supportTs: true
+})
