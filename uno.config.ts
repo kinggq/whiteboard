4d18dbc7a848ef1bad69f9a191a2a50542f9eb0c
kinggq <18701094030@163.com>
2023-09-08 15:00:04 +0800

    chore: update

--- uno.config.ts
+++ uno.config.ts
@@ -1,5 +1,4 @@
-import { defineConfig } from '@unocss/vite'
-import { presetAttributify, presetIcons, presetUno } from 'unocss'
+import { defineConfig, presetAttributify, presetIcons, presetUno } from 'unocss'
 
 export default defineConfig({
   presets: [
